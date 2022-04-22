1493fda97ce3e8c167ccef3b2a2586763ca58341
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-04-22 20:11:09 +0530

    Update Pizza_Ordering_System.py

--- Pizza_Ordering_System.py
+++ Pizza_Ordering_System.py
@@ -1,1 +1,28 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("INVALID INPUT: Please start again!")
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    elif size == "M":
+        bill += 3
+    elif size == "L":
+        bill += 3
+    else:
+        print("INVALID INPUT: Please start again!")
+
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
