ef80b9c87cbd2b40476dec229093c962001be7ff
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-07 19:58:05 +0530

    Commit

---
+++ Heads or Tails.py
@@ -0,0 +1,9 @@
+import random
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+random = random.randint(0,1)
+if random == 1:
+    print("Heads")
+else:
+    print("Tails")
--- Pizza_Ordering_System.py
+++ Pizza_Ordering_System.py
@@ -1,1 +1,32 @@
+<<<<<<< HEAD
 name = input("Hi, welcome to the pizza place, what is your name?")
+=======
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("INVALID INPUT: Please start again!")
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    elif size == "M":
+        bill += 3
+    elif size == "L":
+        bill += 3
+    else:
+        print("INVALID INPUT: Please start again!")
+
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
+>>>>>>> 0e413b6924c9a01245c04e12cbda65f35f9a471e
