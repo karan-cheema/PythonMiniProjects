aa87c1f0fb954699dc16a17f164e30833f621962
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-13 22:27:22 +0530

    Completed Step 2

--- Challenge_1.py
+++ Challenge_1.py
@@ -1,3 +1,6 @@
 import random
 word_list = ["ardvark", "baboon", "camel"]
 random_word = random.choice(word_list)
+
+#To Do-2 - Ask user to guess a letter and assign their answer to a variable called guess. Make guess lowercase
+guess = input("Make a guess: ")
