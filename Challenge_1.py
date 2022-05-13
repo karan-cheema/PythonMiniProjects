26ee30949e79fd5ebbbf2cdb41d213c39a8f9440
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-13 22:32:18 +0530

    Added a cheat code for development purposes

--- Challenge_1.py
+++ Challenge_1.py
@@ -1,6 +1,7 @@
 import random
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+print(f"[HINT] the correct answer is {chosen_word}")
 
 #To Do-2 - Ask user to guess a letter and assign their answer to a variable called guess. Make guess lowercase
 guess = input("Make a guess: ")
