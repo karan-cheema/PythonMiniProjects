e19e65b77abc73da2516f16c27d14bec51dc227b
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-14 19:49:06 +0530

    The guess variable will automatically lowercase any letter

--- HANGMAN - Challenge_1.py
+++ HANGMAN - Challenge_1.py
@@ -4,7 +4,7 @@ chosen_word = random.choice(word_list)
 print(f"[HINT] the correct answer is {chosen_word}")
 
 #To Do-2 - Ask user to guess a letter and assign their answer to a variable called guess. Make guess lowercase
-guess = input("Make a guess: ")
+guess = input("Make a guess: ").lower()
 
 #To Do-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for letter in guess:
