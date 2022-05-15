da6ed09ec36ff5a90741c89440aac804ed972132
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-15 10:59:41 +0530

    Starting code inserted to complete the challenge

---
+++ HANGMAN - Challenge_3.py
@@ -0,0 +1,30 @@
+#Step 2
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+word_length = len(chosen_word)
+
+#Using for loop
+#for letter in chosen_word:
+#    display += "_"
+#print(display)
+
+#Using Range
+for _ in range(word_length):
+    display += "_"
+print(display)
+
+guess = input("Guess a letter: ").lower()
+
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+
+print(display)
