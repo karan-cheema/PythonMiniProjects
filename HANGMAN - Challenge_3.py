cdfd152a13c894202cd1d751ee813dc7800fbc16
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-15 22:51:12 +0530

    Loop added to continue the game

--- HANGMAN - Challenge_3.py
+++ HANGMAN - Challenge_3.py
@@ -19,14 +19,15 @@ for _ in range(word_length):
     display += "_"
 print(display)
 
-
-guess = input("Guess a letter: ").lower()
-
-
-
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 
 print(display)
+
+if "_" not in display:
+    end_of_game = True
