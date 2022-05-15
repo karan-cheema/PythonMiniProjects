8eaa08d6313b15a79ef194d204ebf10b2b5cbfbc
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-15 22:07:20 +0530

    Fixed error

--- HANGMAN - Challenge_3.py
+++ HANGMAN - Challenge_3.py
@@ -1,6 +1,5 @@
-#Step 2
-
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
@@ -20,7 +19,10 @@ for _ in range(word_length):
     display += "_"
 print(display)
 
+
 guess = input("Guess a letter: ").lower()
+
+
 
 for position in range(word_length):
     letter = chosen_word[position]
