7808c05debf51c005619c9ffa34a55fd5488cd48
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-19 22:03:15 +0530

    Program with current position and letter

--- HANGMAN - Challenge_3.py
+++ HANGMAN - Challenge_3.py
@@ -25,9 +25,11 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
     for position in range(word_length):
         letter = chosen_word[position]
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
     print(display)
 
 if "_" not in display:
     end_of_game = True
+    print("You win.")
