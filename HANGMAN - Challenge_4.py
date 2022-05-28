e47bb46b258a9ebd0a58f789d8d8703d695cf193
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-05-28 14:24:02 +0530

    Completed ToDo 2

--- HANGMAN - Challenge_4.py
+++ HANGMAN - Challenge_4.py
@@ -67,6 +67,8 @@ word_length = len(chosen_word)
 #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
 #Set 'lives' to equal 6.
 
+lives = 6
+
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -88,6 +90,12 @@ while not end_of_game:
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    if guess not in chosen_word:
+      lives -= 1
+      if lives == 0:
+        print("You loose!")
+
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
